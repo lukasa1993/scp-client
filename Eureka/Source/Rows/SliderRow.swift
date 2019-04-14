282ec5b53748e2804fe20a1d283baa29e23bb8e4
luka dodelia <luka@picktek.com>
2019-04-14 21:42:07 +0400

    reformat

--- Eureka/Source/Rows/SliderRow.swift
+++ Eureka/Source/Rows/SliderRow.swift
@@ -139,7 +139,7 @@ open class SliderCell: Cell<Float>, CellType {
         contentView.removeConstraints(dynamicConstraints)
         dynamicConstraints = []
         
-        var views: [String : Any] = ["titleLabel": titleLabel!, "slider": slider, "valueLabel": valueLabel!]
+        var views: [String : Any] = ["titleLabel": titleLabel!, "slider": slider!, "valueLabel": valueLabel!]
         let metrics = ["spacing": 15.0]
         valueLabel.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
         titleLabel.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
